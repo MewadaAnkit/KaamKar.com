6f9867df0be6e89251bc4abb00cb681e83d9cdf8
Ankit Mewada <99315107+MewadaAnkit@users.noreply.github.com>
2023-09-30 12:56:32 +0530

    Cors Policy updates

--- Routes/gig.route.js
+++ Routes/gig.route.js
@@ -4,7 +4,7 @@ const createGig = require('../Controller/gig.controller')
 const  verifyToken  = require("../middleware/jwt");
 
 const router = express.Router();
-router.post("/api/gigs/add" , verifyToken,GigController.createGig);
+router.post("/api/gigs" , verifyToken,GigController.createGig);
 router.delete("/api/:id", verifyToken ,GigController.deleteGig);
 router.get("/api/gigs/single/:id", GigController.getGig);
 router.get("/api/gigs", GigController.getGigs);
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require('cors')
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 app.use((err, req, res, next) => {
     const errorStatus = err.status || 500;
     const errorMessage = err.message || "Something went wrong!";
